587aae69edf263553291be5735db27763b012232
Kanika Sharma <kanikash4@gmail.com>
2018-07-30 17:07:30 +0530

    Handling exceptions for creating grid API

--- app/controllers/RouteController.java
+++ app/controllers/RouteController.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Preconditions;
 import models.Edge;
 import models.Vertex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import scala.util.parsing.json.JSONObject;
@@ -18,6 +20,7 @@ import java.util.List;
 
 public class RouteController extends Controller {
 
+    Logger logger = LoggerFactory.getLogger(RouteController.class);
     protected GraphController gc;
     @Inject
     RouteController(GraphController gc){
@@ -36,22 +39,27 @@ public class RouteController extends Controller {
             Vertex location = new Vertex("Node_" + i, "Node_" + i);
             nodes.add(location);
         }
+        try {
+            JsonNode request = request().body().asJson();
+            String id, length, speed, source, destination;
+            Preconditions.checkArgument(request != null, "request is empty or null");
 
-        JsonNode request =request().body().asJson();
-//        ObjectNode response = (ObjectNode) request;
-
-        String id,length, speed, source, destination;
-        Preconditions.checkArgument(request != null, "request is empty or null");
-
-        Iterator<JsonNode> iterator = request.iterator();
-        while (iterator.hasNext()) {
-            JsonNode obj = iterator.next();
-                id = utils.validate.validateAndReturn(obj,"id","id can not be empty or null");
-                length = utils.validate.validateAndReturn(obj,"length","length can not be empty or null");
-                speed =  utils.validate.validateAndReturn(obj, "speed", "speed can not be empty or null");
-                source =  utils.validate.validateAndReturn(obj, "source", "speed can not be empty or null");
-                destination =  utils.validate.validateAndReturn(obj, "destination", "destination can not be empty or null");
-                addLane(id,source,destination,Double.valueOf(length)/Double.valueOf(speed));
+            Iterator<JsonNode> iterator = request.iterator();
+            while (iterator.hasNext()) {
+                JsonNode obj = iterator.next();
+                id = utils.validate.validateAndReturn(obj, "id", "id can not be empty or null");
+                length = utils.validate.validateAndReturn(obj, "length", "length can not be empty or null");
+                speed = utils.validate.validateAndReturn(obj, "speed", "speed can not be empty or null");
+                source = utils.validate.validateAndReturn(obj, "source", "speed can not be empty or null");
+                destination = utils.validate.validateAndReturn(obj, "destination", "destination can not be empty or null");
+                addLane(id, source, destination, Double.valueOf(length) / Double.valueOf(speed));
+            }
+        }catch (IllegalArgumentException ie){
+                    logger.warn("Incorrect Input request : cause: {}", ie.getMessage());
+                    return badRequest("Incorrect Input request");
+        } catch (Exception e) {
+            logger.warn("Error while creating the grid");
+            return badRequest("Error while creating the grid" + e.getMessage());
         }
         return ok("Route Created successfully");
     }
